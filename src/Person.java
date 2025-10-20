267e2f69592a4ed1ac3329107096aad70b26dd4f
Andreea705 <andreeacristina831@gmail.com>
2025-10-20 16:04:51 +0300

    added billigste_tastatur methode

---
+++ src/Person.java
@@ -0,0 +1,22 @@
+public class Person {
+    private String name;
+    private int budget;
+
+    public Person(String name, int budget) {
+        this.name = name;
+        this.budget = budget;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getBudget() {
+        return budget;
+    }
+
+    public void setBudget(int budget) {
+        this.budget = budget;
+    }
+
+}
